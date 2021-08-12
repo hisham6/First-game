073da8897407b90ecdcfc951b46870d9f7d4bcba
hisham6 <hishamalmged@gmail.com>
2021-08-12 15:19:44 +0300

    WaveConfig Scriptable Object

--- Laser Defender/Assets/Scriptes/EnemyPathing.cs
+++ Laser Defender/Assets/Scriptes/EnemyPathing.cs
@@ -5,15 +5,37 @@ using UnityEngine;
 public class EnemyPathing : MonoBehaviour
 {
     [SerializeField] List<Transform> wayPoints;
+    [SerializeField] float moveSpead = 2f;
+    int waypointIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = wayPoints[waypointIndex].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= wayPoints.Count - 1)
+        {
+            var targetPosition = wayPoints[waypointIndex].transform.position;
+            var movementThisFrame = moveSpead * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+           
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
